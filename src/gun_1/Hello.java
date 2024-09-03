eb788f534ef8744c5fe09a888403ad02060c8660
ayce777 <aycee2001coban@gmail.com>
2024-09-03 10:56:09 +0300

    Login testi eklendi(Sana gelen işin adı,Jiradan gelen )

---
+++ src/gun_1/Hello.java
@@ -0,0 +1,16 @@
+package gun_1;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello 1 ");
+
+        System.out.println("Hello 2");
+
+        System.out.println("Task 1 kodları yazıldı.");
+
+
+    }
+}
+// kırmızı: bu dosya henüz takip edilmiyor
+// yeşil renk: bu dosya takip ediliyor, yani değişiklikler kaydedilecek
